ccd9cad85a68fbd6c60aed1b1fb4671ce0713e76
Pandax19 <ajwhitley@comcast.net>
2023-07-31 21:42:32 -0400

    realized mistake -- started wrong project. added correct folders and some starter code

---
+++ models/thoughts.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const thoughtsSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent thought _id
+        thoughtName: {
+            type: String,
+            required: true,
+        },
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // use moment to format date on get
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        // use ReplySchema to validate data for a reply
+        replies: [ReplySchema]
+
+    }, 
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+    
+    ); 
+    const Thought = model('Thought', thoughtsSchema);
+
+    module.exports = Thought;
---
+++ models/user.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+// const moment = require('moment');
+
+// create the User model using the UserSchema
+const User = model('User', userSchema);
+
+const userSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent thought _id
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/]
+        },
+        thoughts: [
+          {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+          }
+        ],
+        friends: [
+          {
+             type: Schema.Types.ObjectId,
+            ref: 'User'
+          }   
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }   
+);
+
+// get total count of friends on retrieval
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+}
+);
+
+
+// export the User model
+module.exports = User;
+
